8e406b07cfa51369078fbc2b067a46653c83661f
jimmyLi <liviu.c.simion@gmail.com>
2019-02-20 18:59:45 +0100

    database connection

---
+++ src/_inc/db.php
@@ -0,0 +1,7 @@
+<?php
+
+$mysqli = new mysqli('mysql', getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'), getenv('MYSQL_PORT'));
+
+if($mysqli->connect_error) {
+  echo 'Connection Error: ' . $mysqli->connect_errno . ' : ' . $mysqli->connect_error;
+}
