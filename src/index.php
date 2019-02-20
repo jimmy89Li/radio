5d723e1df645e2bb0a7bf73b7a6181a9dde4968a
jimmyLi <liviu.c.simion@gmail.com>
2019-02-20 15:30:17 +0100

    fix connection to database

--- src/index.php
+++ src/index.php
@@ -1,11 +1,9 @@
 <?php
 
-echo 'OK!<br>';
-
 $mysqli = new mysqli('mysql', getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'), getenv('MYSQL_PORT'));
 
 if($mysqli->connect_error) {
-  echo 'Connection Error: ' . $mysqli->connect_error;
+  echo 'Connection Error: ' . $mysqli->connect_errno . ' : ' . $mysqli->connect_error;
 } else {
   echo 'Connected succesfully!';
 }
