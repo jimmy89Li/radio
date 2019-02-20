7ade9861b4de42ac679315701842754cacf84dca
jimmyLi <liviu.c.simion@gmail.com>
2019-02-20 13:59:18 +0100

    set-up network, database service and php service

--- src/index.php
+++ src/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Radios!";
+echo 'OK!<br>';
+
+$mysqli = new mysqli('mysql', getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'), getenv('MYSQL_PORT'));
+
+if($mysqli->connect_error) {
+  echo 'Connection Error: ' . $mysqli->connect_error;
+} else {
+  echo 'Connected succesfully!';
+}
